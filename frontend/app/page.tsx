555c5f219c67175d4da671cc6d593e53b1cdac8f
Kevin Wong <kevin.wong954@gmail.com>
2024-08-30 15:47:19 -0400

    add client/server side rendering components

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import Link from 'next/link'
+import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>hello world</h1>
       <Link href="/users">users</Link>
+      <ProductCard />
     </main>
   )
 }
--- frontend/app/users/page.tsx
+++ frontend/app/users/page.tsx
@@ -1,11 +1,26 @@
 import React from 'react'
 
-const page = () => {
+interface User {
+  id: number
+  name: string
+}
+
+const UsersPage = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', {cache: 'no-store'})
+  const users: User[] = await res.json()
   return (
     <main>
-        <h1>users page</h1>
+      <>
+        <h1>Users</h1>
+        <p>{new Date().toLocaleTimeString()}</p>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </>
     </main>
   )
 }
 
-export default page
+export default UsersPage
