eaa35a03a36520a3943c1ad18ff2de9828c9a955
Kevin Wong <kevin.wong954@gmail.com>
2024-09-18 19:37:36 -0400

    add data for weapon skin by id

--- frontend/app/skins/[skinid]/page.tsx
+++ frontend/app/skins/[skinid]/page.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
 import {notFound} from 'next/navigation'
 
-function Skin({params}: {params: {skinid: string}}) {
+const SkinItem = async ({params}: {params: {skinId: string}}) => {
   // if (parseInt(params.skinid) > 10){notFound()}
   // if (isNaN(parseFloat(params.skinid))){notFound()}
 
+  async function fetchSkinItemData(id: string) {
+    const res = await fetch(`https://valorant-api.com/v1/weapons/skins/${id}`)
+    const res_json = await res.json()
+    return res_json
+  }
+
+  const itemData = fetchSkinItemData(params.skinId)
+  console.log('item data: ', itemData)
+
   return (
     <div>
-      <p>Skin id page {params.skinid}</p>
+      <p>Skin id page {params.skinId}</p>
+      <div></div>
     </div>
   )
 }
 
-export default Skin
+export default SkinItem
