4bec5336b805032e028b57c4ca2094a706990d26
Kevin Wong <kevin.wong954@gmail.com>
2024-10-16 14:02:01 -0400

    change loader

--- frontend/app/components/Loader.tsx
+++ frontend/app/components/Loader.tsx
@@ -4,8 +4,9 @@ import spinner from '../../public/images/knife-spin.gif'
 
 const Loader = () => {
   return (
-    <div className="fixed inset-0 flex items-center justify-center">
-      <Image src={spinner} alt="LoadingSpinner" />
+    <div className="fixed inset-0 flex flex-col items-center justify-center">
+      <h1 className="text-white text-4xl font-bold mb-4">Loading...</h1>
+      <Image src={spinner} alt="LoadingSpinner" className="rounded-full object-cover w-96 h-96" />
     </div>
   )
 }
